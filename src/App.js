c05c454943be39d1757c59eebd21fc399f20107b
Seth <Medeirosseth@gamil.com>
2023-06-01 21:04:00 -0700

    deploy: bugfix

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ import "./page.css";
 import Header from "./header/header";
 import Hero from "./hero/hero";
 import Carousel from "./carousel/carousel";
-import Contact from "./contact/contact";
 import Shows from "./shows/shows";
 import Press from "./press/press";
-import Spotify from "./spotify/spotify";
 
 class App extends Component {
   render() {
